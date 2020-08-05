5016d4d227dd1d2f663d721b78328f35a8cfbcd0
Robert Lee <pboymt@foxmail.com>
2020-08-05 11:59:59 +0800

    修改数据格式接口

--- src/interface.ts
+++ src/interface.ts
@@ -32,6 +32,8 @@ export interface IConfig {
     agent_id: string;
     app_key: string;
     app_secret: string;
+    webhook: string;
+    access_token: string;
 }
 
 export interface IDingTalkRequestHeader extends IncomingHttpHeaders {
@@ -39,14 +41,16 @@ export interface IDingTalkRequestHeader extends IncomingHttpHeaders {
     sign?: string;
 }
 
-export interface IDingTalkRequestBody {
+export type IDingTalkRequestBody = IDingTalkRequestBodyGroup | IDingTalkRequestBodyPrivate;
+
+export interface IDingTalkRequestBodyGroup {
     msgtype: 'text';
     text: IDingTalkRequestBodyText;
     msgId: string;
     createAt: number; // ms
     sessionWebhookExpiredTime: number; // ms
     sessionWebhook: string;
-    conversationType: '1' | '2';
+    conversationType: '2'; // 1为私信，2为群对话
     conversationId: string;
     conversationTitle: string; // "钉钉群标题",
     senderId: string;
@@ -58,6 +62,24 @@ export interface IDingTalkRequestBody {
     chatbotUserId: string;
     atUsers: IDingTalkRequestBodyAtUser[];
     isInAtList: boolean;
+}
+
+export interface IDingTalkRequestBodyPrivate {
+    msgtype: 'text';
+    text: IDingTalkRequestBodyText;
+    msgId: string;
+    createAt: number; // ms
+    sessionWebhookExpiredTime: number; // ms
+    sessionWebhook: string;
+    conversationType: '1'; // 1为私信，2为群对话
+    conversationId: string;
+    senderId: string;
+    senderNick: string;
+    isAdmin: boolean;
+    senderCorpId: string;
+    senderStaffId: string; // userid
+    chatbotCorpId: string;
+    chatbotUserId: string;
 }
 
 export interface IDingTalkRequestBodyText {
