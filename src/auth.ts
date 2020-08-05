b36292b07f9198ebc1b71307ac4774fa9e999e83
Robert Lee <pboymt@foxmail.com>
2020-08-05 15:28:47 +0800

    添加启动通知

--- src/auth.ts
+++ src/auth.ts
@@ -8,3 +8,17 @@ export function verify_service_factory(secret: string) {
         return hmac.update(string_to_sign, 'utf8').digest().toString('base64') === sign;
     }
 }
+
+export function webhook_sign_factory(secret: string) {
+    const app_secret = secret;
+    return function () {
+        const timestamp = Date.now();
+        const hmac = createHmac('sha256', secret);
+        const string_to_sign = timestamp + '\n' + app_secret;
+        const sign = hmac.update(string_to_sign, 'utf8').digest().toString('base64');
+        return {
+            timestamp,
+            sign
+        };
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import Express, { json } from 'express';
 import { IDingTalkResponse, IConfig, IDingTalkRequestHeader, IDingTalkRequestBody } from './interface';
 import { readFileSync, existsSync } from "fs";
 import { join } from "path";
-import { verify_service_factory } from './auth';
+import { verify_service_factory, webhook_sign_factory } from './auth';
 import { switch_response } from './response';
+import Axios from "axios";
+import { parse } from "url";
 
 const config_path = join(__dirname, '../config.json');
 if (!existsSync(config_path)) {
@@ -14,6 +16,7 @@ if (!existsSync(config_path)) {
 const config: IConfig = JSON.parse(readFileSync(config_path, 'utf-8'));
 
 const verify_request = verify_service_factory(config.app_secret);
+const webhook_sign = webhook_sign_factory(config.app_secret);
 
 const app = Express();
 
@@ -50,4 +53,19 @@ app.post('/bot', async (req, res) => {
 
 app.listen(3720, () => {
     console.log('listening...');
+    const url = new URL(config.webhook);
+    const sign = webhook_sign();
+    url.searchParams.append('timestamp', sign.timestamp.toString());
+    url.searchParams.append('sign', sign.sign);
+    Axios.post(url.toString(), {
+        "msgtype": "markdown",
+        "markdown": {
+            "title": "启动通知",
+            "text": readFileSync(join(__dirname, '../statics/changelogs.md'), 'utf8')
+        },
+        "at": {
+            "atMobiles": [],
+            "isAtAll": false
+        }
+    })
 });
