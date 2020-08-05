c508a7fb9497f5afe98b48d528445eb50beeb3de
Robert Lee <pboymt@foxmail.com>
2020-08-05 10:37:30 +0800

    支持查询今明日程

--- src/response/calendar.ts
+++ src/response/calendar.ts
@@ -11,7 +11,7 @@ export async function response_calendar(): Promise<IDingTalkResponse> {
         const now = moment().utcOffset(480);
         if (now.isBefore(today, 'hour')) { // 判断是否到4点
             now.subtract(1, "day");
-            content += '现在还未到下一个PCR日期\n\n';
+            content += '现在是凌晨时段，现在还未到下一个PCR日期，请注意休息\n\n';
         }
         try {
             const today_events = calendar[now.year()][now.month() + 1][now.date()];
@@ -53,6 +53,95 @@ export async function response_calendar(): Promise<IDingTalkResponse> {
             if (today_events.jssr.length) {
                 content += `角色生日：\n${today_events.jssr}\n`;
             }
+        }
+    }
+    return {
+        msgtype: 'text',
+        text: {
+            content
+        },
+        at: {
+            atMobiles: [],
+            isAtAll: false
+        }
+    }
+}
+
+export async function response_today_event(): Promise<IDingTalkResponse> {
+    const calendar = await get_canlendar();
+    let content = '获取失败';
+    if (calendar) {
+        content = '';
+        const today = moment().utcOffset(480).hours(4);
+        const now = moment().utcOffset(480);
+        if (now.isBefore(today, 'hour')) { // 判断是否到4点
+            now.subtract(1, "day");
+            content += '现在是凌晨时段，还未到下一个PCR日期，请注意休息\n\n';
+        }
+        try {
+            const today_events = calendar[now.year()][now.month() + 1][now.date()];
+            content += `今日活动\n`;
+            if (today_events.qdhd.length) {
+                content += `庆典活动：\n${today_events.qdhd}\n`;
+            }
+            if (today_events.jqhd.length) {
+                content += `剧情活动：\n${today_events.jqhd}\n`;
+            }
+            if (today_events.tbhd.length) {
+                content += `特别活动：\n${today_events.tbhd}\n`;
+            }
+            if (today_events.tdz.length) {
+                content += `团队战：\n${today_events.tdz}\n`;
+            }
+            if (today_events.jssr.length) {
+                content += `角色生日：\n${today_events.jssr}\n`;
+            }
+        } catch {
+
+        }
+    }
+    return {
+        msgtype: 'text',
+        text: {
+            content
+        },
+        at: {
+            atMobiles: [],
+            isAtAll: false
+        }
+    }
+}
+
+export async function response_tomorrow_event(): Promise<IDingTalkResponse> {
+    const calendar = await get_canlendar();
+    let content = '获取失败';
+    if (calendar) {
+        content = '';
+        const today = moment().utcOffset(480).hours(4);
+        const tomorrow = moment().utcOffset(480);
+        if (tomorrow.isAfter(today, 'hour')) { // 判断是否到4点
+            tomorrow.add(1, "day");
+        }
+        try {
+            const tomorrow_events = calendar[tomorrow.year()][tomorrow.month() + 1][tomorrow.date()];
+            content += `明日活动\n`;
+            if (tomorrow_events.qdhd.length) {
+                content += `庆典活动：\n${tomorrow_events.qdhd}\n`;
+            }
+            if (tomorrow_events.jqhd.length) {
+                content += `剧情活动：\n${tomorrow_events.jqhd}\n`;
+            }
+            if (tomorrow_events.tbhd.length) {
+                content += `特别活动：\n${tomorrow_events.tbhd}\n`;
+            }
+            if (tomorrow_events.tdz.length) {
+                content += `团队战：\n${tomorrow_events.tdz}\n`;
+            }
+            if (tomorrow_events.jssr.length) {
+                content += `角色生日：\n${tomorrow_events.jssr}\n`;
+            }
+        } catch {
+
         }
     }
     return {
--- src/response/index.ts
+++ src/response/index.ts
@@ -1,31 +1,31 @@
 import { IDingTalkRequestBody, IDingTalkResponse } from "../interface";
-import { response_calendar } from "./calendar";
+import { response_calendar, response_today_event } from "./calendar";
 
 export async function switch_response(msg: IDingTalkRequestBody): Promise<IDingTalkResponse> {
     const msg_text = msg.text.content.trim();
     if (msg_text === '') {
-        return {
-            msgtype: 'text',
-            text: {
-                content: '你倒是说句话啊！'
-            },
-            at: {
-                atMobiles: [],
-                isAtAll: false
-            }
-        }
-    } else if (/^日程表/.test(msg_text)) {
+        return empty_response('你倒是说句话啊！');
+    } else if (['日程表', '近期活动'].includes(msg_text)) {
         return await response_calendar();
-    } else {
-        return {
-            msgtype: 'text',
-            text: {
-                content: '你在说啥？'
-            },
-            at: {
-                atMobiles: [],
-                isAtAll: false
-            }
+    } else if (['今天', '今日', '今天日程', '今日日程', '今天活动', '今日活动', '活动'].includes(msg_text)) {
+        return await response_today_event();
+    } else if (['明天', '明日', '明天日程', '明日日程', '明天活动', '明日活动'].includes(msg_text)) {
+        return await response_today_event();
+    }
+    else {
+        return empty_response('你在说啥？');
+    }
+}
+
+function empty_response(msg: string): IDingTalkResponse {
+    return {
+        msgtype: 'text',
+        text: {
+            content: msg
+        },
+        at: {
+            atMobiles: [],
+            isAtAll: false
         }
     }
 }
