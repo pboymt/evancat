be51c52bc71776b4763681f53d0250ab0d34a81e
Robert Lee <pboymt@foxmail.com>
2020-08-07 22:53:01 +0800

    抓取错误

--- src/response/index.ts
+++ src/response/index.ts
@@ -6,23 +6,27 @@ import { response_latest_news, response_news_list } from "./news";
 export async function switch_response(msg: IDingTalkRequestBody): Promise<IDingTalkResponse> {
     const msg_text = msg.text.content.trim().replace(/\s{2,}/, ' ');
     const args = msg_text.split(' ');
-    if (args[0] === '') {
-        return empty_response('你倒是说句话啊！');
-    } else if (/^(帮助|help)/.test(args[0])) {
-        return response_help(args);
-    } else if (['日程表', '近期活动'].includes(args[0])) {
-        return await response_calendar();
-    } else if (['今天', '今日', '今天日程', '今日日程', '今天活动', '今日活动', '活动'].includes(args[0])) {
-        return await response_today_event();
-    } else if (['明天', '明日', '明天日程', '明日日程', '明天活动', '明日活动'].includes(args[0])) {
-        return await response_tomorrow_event();
-    } else if (['新闻', '最新新闻'].includes(args[0])) {
-        return await response_latest_news(args);
-    } else if (['新闻列表'].includes(args[0])) {
-        return await response_news_list(args);
-    }
-    else {
-        return empty_response('你在说啥？');
+    try {
+        if (args[0] === '') {
+            return empty_response('你倒是说句话啊！');
+        } else if (/^(帮助|help)/.test(args[0])) {
+            return response_help(args);
+        } else if (['日程表', '近期活动'].includes(args[0])) {
+            return await response_calendar();
+        } else if (['今天', '今日', '今天日程', '今日日程', '今天活动', '今日活动', '活动'].includes(args[0])) {
+            return await response_today_event();
+        } else if (['明天', '明日', '明天日程', '明日日程', '明天活动', '明日活动'].includes(args[0])) {
+            return await response_tomorrow_event();
+        } else if (['新闻', '最新新闻'].includes(args[0])) {
+            return await response_latest_news(args);
+        } else if (['新闻列表'].includes(args[0])) {
+            return await response_news_list(args);
+        }
+        else {
+            return empty_response('你在说啥？');
+        }
+    } catch (err) {
+        return empty_response(String(err));
     }
 }
 
